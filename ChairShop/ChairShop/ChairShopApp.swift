e1695bd5856371da81faed2eabd868d0347d84e1
Berkan Aydın <106911385+berkan2323@users.noreply.github.com>
2025-04-23 22:34:51 +0300

    Add files via upload

---
+++ ChairShop/ChairShop/ChairShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChairShopApp.swift
+//  ChairShop
+//
+//  Created by Berkan Aydın on 23.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChairShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
